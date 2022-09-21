7c50d46b2e4ca93c93eadaa7257a2043974e2d10
Franco <francozp101110@gmail.com>
2022-09-21 09:27:50 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+        div#grid_container{
+        width:900px;
+        margin:0px auto;
+        height:860px;
+        border:#999 1px dashed;
+    }
+        
+    </style>
+
+
+
+</head>
+<body>
+
+
+
+
+<div id="grid_container">
+    <div style="height: 140px;"><div>Hidrogeno1</div></div>
+        <div style="height: 140px;"><div>Litio3</div></div>
+        <div style="height: 140px;"><div>Berilio4</div></div>
+        <div style="height: 140px;"><div>Sodio11</div></div>
+
+        </div>
+</body>
+</html>
